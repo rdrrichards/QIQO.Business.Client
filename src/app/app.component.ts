a613ec4ce5f5c54dd616999524344ba1751ebbe4
R. Richards <rdrrichards@hotmail.com>
2018-11-15 15:01:34 -0600

    ng updates to latest

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'qiqo-business-client';
   ver = VERSION.full;
 }
