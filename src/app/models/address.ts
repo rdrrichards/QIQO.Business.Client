e0ab4df3ed2d8d485a7c17152fabbf1255ebfb22
R. Richards <rdrrichards@hotmail.com>
2019-08-18 16:28:18 -0500

    address models

---
+++ src/app/models/address.ts
@@ -0,0 +1,36 @@
+import { AddressType, EntityType } from './enums';
+
+export interface Address {
+  addressKey: number;
+  addressType: AddressType;
+  addressTypeData: AddressTypeData;
+  entityKey: number;
+  entityType: EntityType;
+  addressLine1: string;
+  addressLine2: string;
+  addressLine3: string;
+  addressLine4: string;
+  addressCity: string;
+  addressState: string;
+  addressCounty: string;
+  addressCountry: string;
+  addressPostalCode: string;
+  addressNotes: string;
+  addressDefaultFlag: boolean;
+  addressActiveFlag: boolean;
+  addedUserID: string;
+  addedDateTime: Date;
+  updateUserID: string;
+  updateDateTime: Date;
+}
+
+export interface AddressTypeData {
+  addressTypeKey: number;
+  addressTypeCode: string;
+  addressTypeName: string;
+  addressTypeDesc: string;
+  addedUserID: string;
+  addedDateTime: Date;
+  updateUserID: string;
+  updateDateTime: Date;
+}
