e725423dd6c96932380fc854093ee2fc77018985
R. Richards <rdrrichards@hotmail.com>
2019-08-19 18:38:12 -0500

    shared components scaffold

---
+++ src/app/shared/busy/busy.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'qiqo-busy',
+  templateUrl: './busy.component.html',
+  styleUrls: ['./busy.component.css']
+})
+export class BusyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/shared/list-detail/list-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'qiqo-list-detail',
+  templateUrl: './list-detail.component.html',
+  styleUrls: ['./list-detail.component.css']
+})
+export class ListDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
