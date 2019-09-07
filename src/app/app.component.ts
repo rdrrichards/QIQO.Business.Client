7f00965406a53e681063e8e4570fb6c5b9516816
R. Richards <rdrrichards@hotmail.com>
2019-09-07 18:06:23 -0500

    clean up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 import { Store, select } from '@ngrx/store';
 import { AppState } from './state';
-import * as appActions from './state/app.actions';
 import * as appReducer from './state/app.reducer';
 import { Observable } from 'rxjs';
-import { User } from './models/user';
 
 @Component({
   selector: 'qiqo-root',
@@ -14,7 +12,6 @@ import { User } from './models/user';
 export class AppComponent {
   loggedIn$: Observable<boolean>;
   constructor(private store: Store<AppState>) {
-    // this.store.dispatch(appActions.fetchUser());
     this.loggedIn$ = this.store.pipe(select(appReducer.selectLoggedIn));
   }
 }
