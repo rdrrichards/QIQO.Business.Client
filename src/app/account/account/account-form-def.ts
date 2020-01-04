5ac8c1cdc4f7732c1db5f4668d592819612c675a
R. Richards <rdrrichards@hotmail.com>
2020-01-04 16:18:17 -0600

    dynamic form updates

--- src/app/account/account/account-form-def.ts
+++ src/app/account/account/account-form-def.ts
@@ -10,11 +10,16 @@ export class AccountFormDefinition {
     { key: 'accountKey', type: 'number', isId: true, validators: [ Validators.required ] },
     { key: 'companyKey', type: 'number', isId: true, validators: [ Validators.required ] },
     { key: 'accountType', type: 'select', label: 'Type', validators: [ Validators.required ], options: this.accountTypes },
-    { key: 'accountCode', type: 'string', label: 'Code', validators: [ Validators.required ], fieldWidth: '15%' },
-    { key: 'accountName', type: 'string', label: 'Name', validators: [ Validators.required ], fieldWidth: '50%' },
-    { key: 'accountDesc', type: 'string', label: 'Description', validators: [ Validators.required ], fieldWidth: '100%' },
-    { key: 'accountStartDate', type: 'date', label: 'Start Date', validators: [ Validators.required ], fieldWidth: '15%' },
-    { key: 'accountEndDate', type: 'date', label: 'End Date', fieldWidth: '15%' }
+    { key: 'accountCode', type: 'string', label: 'Code',
+      validators: [ Validators.required, Validators.maxLength(10) ],
+      fieldWidth: '15%' },
+    { key: 'accountName', type: 'string', label: 'Name',
+      validators: [ Validators.required, Validators.maxLength(100) ],
+      fieldWidth: '50%' },
+    { key: 'accountDesc', type: 'string', label: 'Description',
+      validators: [ Validators.required, Validators.maxLength(255) ], fieldWidth: '100%' },
+    { key: 'accountStartDate', type: 'date', label: 'Start Date', validators: [ Validators.required ] },
+    { key: 'accountEndDate', type: 'date', label: 'End Date' }
   ];
   accountQuickCreateFormDefinition: FieldDefinition[] = [
     { key: 'accountType', type: 'select', label: 'Type', validators: [ Validators.required ], options: this.accountTypes },
