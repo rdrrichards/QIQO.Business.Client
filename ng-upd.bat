a7118f8bf4fa1ed81682de9f3ba8017b611c4a17
R. Richards <rdrrichards@hotmail.com>
2020-01-30 17:20:29 -0600

    pre update commit

--- ng-upd.bat
+++ ng-upd.bat
@@ -5,4 +5,6 @@ CALL ng update @angular/cli@next --force
 CALL git commit -am "cli update to latest"
 CALL npm audit fix
 CALL git commit -am "post update commit"
+CALL rm -r -fo .\node_modules\
+CALL npm i
 CALL bt.bat
