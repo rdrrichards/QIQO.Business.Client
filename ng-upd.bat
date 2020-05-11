1ea2cca0ed9c6a6336601594019c3689cd876a45
R. Richards <rdrrichards@hotmail.com>
2020-05-11 18:52:46 -0500

    post update to latest commit

--- ng-upd.bat
+++ ng-upd.bat
@@ -3,6 +3,8 @@ CALL ng update @angular/core
 CALL git commit -am "ng update to latest"
 CALL ng update @angular/cli
 CALL git commit -am "cli update to latest"
+CALL ng update @angular/cdk
+CALL git commit -am "cdk update to latest"
 CALL npm audit fix
 CALL git commit -am "post update commit"
 REM CALL rm -r -fo .\node_modules\
